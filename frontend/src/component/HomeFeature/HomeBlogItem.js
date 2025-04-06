f9ef424a7611ee2df81f36bdb1aba245860c4b17
buivantoana <toanbui219@gmail.com>
2025-04-06 14:04:10 +0700

    .

--- frontend/src/component/HomeFeature/HomeBlogItem.js
+++ frontend/src/component/HomeFeature/HomeBlogItem.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 function HomeBlogItem(props) {
     return (
-        <div className="col-lg-4 col-md-6">
-            <div className="single-blog">
+        <div className="col-lg-4 col-md-6" >
+            <div className="single-blog" style={{ border: "1px solid #ddd", borderRadius: "5px" }}>
                 <div className="thumb">
-                    <img style={{width:'350px', height:'243px', objectFit:'cover', cursor:'pointer'}} className="img-fluid" src={props.data.image} alt="" />
+                    <img style={{ height: '243px', objectFit: 'cover', cursor: 'pointer', borderTopLeftRadius: "5px", borderTopRightRadius: "5px" }} className="img-fluid" src={props.data.image} alt="" />
                 </div>
-                <div className="short_details">
+                <div className="short_details" style={{ padding: "10px" }}>
                     <div className="meta-top d-flex">
-                        <a >{props.data.userData.firstName +" "+props.data.userData.lastName}</a>
+                        <a >Tác giả: {props.data.userData.firstName + " " + props.data.userData.lastName}</a>
                         <a ><i className="ti-comments-smiley" />{props.data.commentData.length} Bình luận</a>
                     </div>
                     <Link className="d-block" to={`/blog-detail/${props.data.id}`}>
@@ -21,7 +21,7 @@ function HomeBlogItem(props) {
                             {props.data.description}
                         </p>
                     </div>
-                    <a  className="blog_btn">Xem thêm<span className="ml-2 ti-arrow-right" /></a>
+                    <a className="blog_btn">Xem thêm<span className="ml-2 ti-arrow-right" /></a>
                 </div>
             </div>
         </div>
